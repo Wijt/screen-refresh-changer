36007feaf7e47740f2f7b4977db9ebb67a2229ec
Furkan Kaya <8fkaya@gmail.com>
2020-12-02 04:57:56 +0300

    Configs class added to the project.
    In this way, the application configs can be changed through the configs.json file.

---
+++ ScreenFixer/ScreenFixer/Configs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ScreenFixer
+{
+    class Configs
+    {
+        public string depth, rRate1, rRate2, width, height;
+        public bool debugMode;
+        public Configs(string configFileName = "configs.json")
+        {
+            Configs.Update(this, configFileName);
+        }
+
+        static void Update(Configs configs, string fileName)
+        {
+            string configJSON = File.ReadAllText(fileName);
+            JObject configsInFile = JObject.Parse(configJSON);
+            configs.width = (string)configsInFile["ScreenSize"]["Width"];
+            configs.height = (string)configsInFile["ScreenSize"]["Height"];
+            configs.depth = (string)configsInFile["Depth"];
+            configs.rRate1 = (string)configsInFile["RefreshRateOne"];
+            configs.rRate2 = (string)configsInFile["RefreshRateTwo"];
+            configs.debugMode = (bool)configsInFile["DebugMode"];
+        }
+        public override string ToString()
+        {
+            return width + "x" + height + " " + depth + " " + rRate1 + " " + rRate2;
+        }
+    }
+}
--- ScreenFixer/ScreenFixer/Program.cs
+++ ScreenFixer/ScreenFixer/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+
 namespace ScreenFixer
 {
     static class Program
@@ -9,6 +10,8 @@ namespace ScreenFixer
         static void Main()
         {
             string previousRefreshRate = File.ReadAllText("second-screen-refresh-rate.txt");
+            Configs configs = new Configs();
+
             string deviceName = "";
             if (Screen.AllScreens.Length == 2)
             {
@@ -16,24 +19,32 @@ namespace ScreenFixer
                 Console.WriteLine("Second device name: " + deviceName + ", Refreshrate: " + previousRefreshRate);
             }
 
-            previousRefreshRate = previousRefreshRate == "60" ? "50" : "60";
+            previousRefreshRate = previousRefreshRate == configs.rRate1 ? configs.rRate2 : configs.rRate1;
             Console.WriteLine("New refreshrate: " + previousRefreshRate);
 
             var StartInfo = new ProcessStartInfo
             {
                 FileName = @"C:\Program Files (x86)\12noon Display Changer\dc64.exe",
-                Arguments = " -monitor=\"" + deviceName + "\" -width=1920 -height=1080 -refresh=" + previousRefreshRate + " -depth=32",
+                Arguments = " -monitor=\"" + deviceName + "\" -width=" + configs.width + " -height=" + configs.height + " -refresh=" + previousRefreshRate + " -depth=" + configs.depth,
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 CreateNoWindow = false,
                 WorkingDirectory = @"C:\Program Files (x86)\12noon Display Changer\"
             };
+
             using (Process process = Process.Start(StartInfo))
             {
                 process.WaitForExit();
             }
-            Console.WriteLine("-------------------------------");
             File.WriteAllText("second-screen-refresh-rate.txt", previousRefreshRate);
+            if (configs.debugMode)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine(configs.ToString());
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
-    } 
+    }
 }
+
